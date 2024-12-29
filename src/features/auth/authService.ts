5ebb02914f6aaba81e1e7d9a015f5ab7a9d8b250
Rohit Nijjar <50916372+RohitNijjar@users.noreply.github.com>
2024-12-29 17:28:58 +0800

    Fixed compare password (#4)

--- src/features/auth/authService.ts
+++ src/features/auth/authService.ts
@@ -1,9 +1,10 @@
-import { HTTP_STATUS } from "../../shared/constants";
+import { ERROR_CODES, HTTP_STATUS } from "../../shared/constants";
 import { AppError } from "../../shared/errors";
 import { comparePassword, hashPassword } from "../../shared/utils/jwt/hash";
 import { generateToken } from "../../shared/utils/jwt/jwt";
-import { toUser, toUserPayload } from "./authMappers";
+import { toUser, toUserDto, toUserPayload } from "./authMappers";
 import { AuthRepository } from "./authRepository";
+import { UserDTO } from "./dtos/userDto";
 import { AUTH_ERROR_CODES } from "./errorCodes";
 import { User } from "./models/userModel";
 
@@ -13,26 +14,29 @@ export const AuthService = {
         password: string,
         firstName: string,
         lastName: string,
-    ): Promise<User> => {
+    ): Promise<UserDTO> => {
         const hashedPassword = await hashPassword(password);
-
         const newUser = await AuthRepository.register(toUser(email, hashedPassword, firstName, lastName, false));
-        return newUser;
+        
+        return toUserDto(newUser);
     },
 
-    login: async (email: string, password: string): Promise<string> => { 
+    login: async (email: string, password: string): Promise<UserDTO> => { 
         const user = await AuthRepository.getUserByEmail(email);
-        
         if (!user) {
             throw new AppError("Auth service error", AUTH_ERROR_CODES.INVALID_CREDENTIALS, HTTP_STATUS.UNAUTHORIZED);
         }
 
-        const isPasswordValid = await comparePassword(password);
-
+        const isPasswordValid = await comparePassword(password, user.password);
         if(!isPasswordValid) {
             throw new AppError("Auth service error", AUTH_ERROR_CODES.INVALID_CREDENTIALS, HTTP_STATUS.UNAUTHORIZED);
         }
 
-        return generateToken(toUserPayload(user.id!, user.email, user.isVerified));
+        if (!user.id) {
+            throw new AppError("Auth service error", ERROR_CODES.INTERNAL_SERVER_ERROR);
+        }
+        const token = generateToken(toUserPayload(user.id, user.email, user.isVerified));
+
+        return toUserDto(user, token);
     },
 };
---
+++ src/features/auth/dtos/userDTO.ts
@@ -0,0 +1,8 @@
+export interface UserDTO { 
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    isVerified: boolean;
+    token?: string;
+}
