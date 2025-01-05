834c4fddc62479f66bef13062d42aca4179bfec4
Rohit Nijjar <rohitnijjar@gmail.com>
2025-01-05 15:04:46 +0800

    Implemented logout and refresh token

--- src/features/auth/controllers/authController.ts
+++ src/features/auth/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
+import { env } from '../../../config';
 import { ERROR_CODES, HTTP_STATUS } from '../../../shared/constants';
 import { ApiError } from '../../../shared/errors';
 import { createApiResponse } from '../../../shared/utils/responseHandler';
@@ -40,7 +41,15 @@ export const AuthController = {
   ): Promise<void> => {
     const { email, password } = req.body;
     try {
-      const user = await AuthService.login(toLoginRequest(email, password));
+      const { user, refreshToken } = await AuthService.login(
+        toLoginRequest(email, password),
+      );
+
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+      });
 
       res.status(HTTP_STATUS.OK).json(
         createApiResponse({
@@ -149,6 +158,43 @@ export const AuthController = {
         createApiResponse({
           data: {
             message: 'Verification email sent successfully',
+          },
+          statusCode: HTTP_STATUS.OK,
+        }),
+      );
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  logout: async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const refreshToken = req.cookies.refreshToken;
+
+      if (!refreshToken) {
+        throw new ApiError(
+          'Auth controller error: No refresh token',
+          ERROR_CODES.NO_REFRESH_TOKEN,
+          HTTP_STATUS.UNAUTHORIZED,
+        );
+      }
+
+      await AuthService.logout(refreshToken);
+
+      res.clearCookie('refreshToken', {
+        httpOnly: true,
+        secure: env.NODE_ENV === 'production',
+        sameSite: 'strict',
+      });
+
+      res.status(HTTP_STATUS.OK).json(
+        createApiResponse({
+          data: {
+            message: 'Logged out successfully',
           },
           statusCode: HTTP_STATUS.OK,
         }),
--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './httpStatus';
 export * from './errorCodes';
+export * from './jwt';
