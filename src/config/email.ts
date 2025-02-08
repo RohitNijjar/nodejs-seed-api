134fe0f1b19c1ddb1f559939cc3f2735022a5123
Rohit Nijjar <50916372+RohitNijjar@users.noreply.github.com>
2025-02-08 16:40:36 +0800

    Updated .env.sample and nodeMailer config
    * Updated .env.sample and nodeMailer config
    
    * Updated yaml

--- src/config/email.ts
+++ src/config/email.ts
@@ -4,6 +4,7 @@ import { env } from './env';
 
 const transporter = nodemailer.createTransport({
   service: env.EMAIL_SERVICE,
+  secure: env.NODE_ENV === 'production',
   auth: {
     user: env.ADMIN_EMAIL,
     pass: env.ADMIN_PASSWORD,
